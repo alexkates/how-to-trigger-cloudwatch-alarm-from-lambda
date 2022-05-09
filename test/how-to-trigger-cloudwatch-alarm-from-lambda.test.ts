d4fa102443a4b946dfad3d1dc1948d903915fef6
Alex Kates <alex@Alexs-MBP.verizon.net>
2022-05-09 09:30:35 -0400

    Initial commit

---
+++ test/how-to-trigger-cloudwatch-alarm-from-lambda.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HowToTriggerCloudwatchAlarmFromLambda from '../lib/how-to-trigger-cloudwatch-alarm-from-lambda-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/how-to-trigger-cloudwatch-alarm-from-lambda-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HowToTriggerCloudwatchAlarmFromLambda.HowToTriggerCloudwatchAlarmFromLambdaStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
